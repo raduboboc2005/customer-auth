2d45a49397470dfb86e9891b943da3fa95be508c
Boboc Radu <radu.gabriel.boboc@fisglobal.com>
2021-11-03 13:12:10 +0300

    first commit

---
+++ src/main/java/com/dummy/authentication/DummyAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.dummy.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dummy/authentication/DummyAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummy.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
