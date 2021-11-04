1e4bbcaacdbd417077105c43d80dae38db733a9c
Boboc Radu <radu.gabriel.boboc@fisglobal.com>
2021-11-04 10:42:49 +0300

    added user and user_details tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER (USERNAME, PASSWORD) VALUES ('erika.muller29', 'pass1');
+INSERT INTO USER (USERNAME, PASSWORD) VALUES ('thomas.jef11', 'pass2');
+
+INSERT INTO USER_DETAILS (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) VALUES('erika.muller29', 'Erika', 'Muller', 'erikamuller@gmail.com');
+INSERT INTO USER_DETAILS (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) VALUES('thomas.jef11', 'Thomas', 'Jeff', 'thom.jef@gmail.com');
